e352eacfee1418a1a92c00fcdde56e2530ead182
Ivan Zuev <ivan.zuev.1984@mail.ru>
2023-10-15 21:54:00 +0300

    100% покрытитя

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,11 +2,19 @@ from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], 0, "test") == "test"
-    assert arrs.get([1, 2, 3, 4, 6, 5],7, None) == None
+    assert arrs.get([1, 2, 3], 1, None) == 2
+    assert arrs.get([], 7, None) == None
+
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7, 8],3, -1) == [4, 5, 6, 7]
+    assert arrs.my_slice([1, 2, 3], 0, -1) == [1, 2]
+    assert arrs.my_slice([1, 2, 3], 0, 0) == []
+    assert arrs.my_slice([1, 2, 3, 4], -1) == [4]
+    assert arrs.my_slice([1, 2, 3, 4], 5) == []
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7, 8], 3, -1) == [4, 5, 6, 7]
+    assert arrs.my_slice([1, 2, 3, 4], end=0) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7, 8], -1, 9) == [8]
+    assert arrs.my_slice([1, 2, 3]) == [1, 2, 3]
